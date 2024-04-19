af896ba6c646c10c531a88e18e6aea429aa06e05
yusufsham <yusufsham@hotmail.com>
2024-04-19 17:21:17 +0530

    Javascript code

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,80 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules } from "react-native";
+import type { ComScoreParams, Data1p } from "./utility/type";
+const { ComScoreModule } = NativeModules;
 
-const LINKING_ERROR =
-  `The package 'react-native-comscore' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+export async function initializeComscore(params: ComScoreParams) {
+  try {
+    const comScoreParams: ComScoreParams = {
+      publisherId: params.publisherId,
+    };
 
-const Comscore = NativeModules.Comscore
-  ? NativeModules.Comscore
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+    if (params.applicationName !== undefined) {
+      comScoreParams.applicationName = params.applicationName;
+    }
 
-export function multiply(a: number, b: number): Promise<number> {
-  return Comscore.multiply(a, b);
+    if (params.usagePropertiesAutoUpdateMode !== undefined) {
+      comScoreParams.usagePropertiesAutoUpdateMode =
+        params.usagePropertiesAutoUpdateMode;
+    }
+
+    if (params.usagePropertiesAutoUpdateInterval !== undefined) {
+      comScoreParams.usagePropertiesAutoUpdateInterval =
+        params.usagePropertiesAutoUpdateInterval;
+    }
+
+    if (params.data_1p !== undefined) {
+      comScoreParams.data_1p = params.data_1p;
+    }
+
+    await ComScoreModule.initializeComScore(comScoreParams);
+
+    console.log("comScoreInitialized");
+  } catch (e) {
+    console.log("Error in initializing comscore", e);
+  }
+}
+
+export async function trackScreen(screenName: String) {
+  try {
+    if (screenName === undefined) return;
+    await ComScoreModule.trackScreen(screenName);
+  } catch (e) {
+    console.log("error in trackScreen : ", e);
+  }
+}
+
+export async function updateConsent(consentState: string) {
+  try {
+    await ComScoreModule.updateConsent(consentState);
+  } catch (e) {
+    console.log("Error in updateConsent", e);
+  }
+}
+
+export async function updateData1P(params: Data1p) {
+  let updatedData1p: Data1p = {};
+
+  if (params.publisherId !== undefined) {
+    updatedData1p.publisherId = params.publisherId;
+  } else {
+    return console.error("PublisherId is required!");
+  }
+
+  if (params.cs_fpid !== undefined) {
+    updatedData1p.cs_fpid = params.cs_fpid;
+  }
+
+  if (params.cs_fpdm !== undefined) {
+    updatedData1p.cs_fpdm = params.cs_fpdm;
+  }
+
+  if (params.cs_fpit !== undefined) {
+    updatedData1p.cs_fpit = params.cs_fpit;
+  }
+
+  if (params.cs_fpdt !== undefined) {
+    updatedData1p.cs_fpdt = params.cs_fpdt;
+  }
+
+  await ComScoreModule.update1PData(updatedData1p);
 }
