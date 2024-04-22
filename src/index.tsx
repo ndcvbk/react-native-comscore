6b2627898858227fb234a601fdae12bf2d1054d6
yusufsham <yusufsham@hotmail.com>
2024-04-22 11:29:25 +0530

    Some important fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,80 +1,99 @@
 import { NativeModules } from 'react-native';
+import { useState } from 'react';
 import type { ComScoreParams, Data1p } from './utility/type';
+
 const { ComScoreModule } = NativeModules;
 
-export async function initializeComscore(params: ComScoreParams) {
-  try {
-    const comScoreParams: ComScoreParams = {
-      publisherId: params.publisherId,
-    };
+const useComscore = () => {
+  const [comscoreInstance] = useState(() => {
+    class Comscore {
+      async initializeComscore(params: ComScoreParams) {
+        try {
+          const comScoreParams: ComScoreParams = {
+            publisherId: params.publisherId,
+          };
 
-    if (params.applicationName !== undefined) {
-      comScoreParams.applicationName = params.applicationName;
+          if (params.applicationName !== undefined) {
+            comScoreParams.applicationName = params.applicationName;
+          }
+
+          if (params.usagePropertiesAutoUpdateMode !== undefined) {
+            comScoreParams.usagePropertiesAutoUpdateMode =
+              params.usagePropertiesAutoUpdateMode;
+          }
+
+          if (params.usagePropertiesAutoUpdateInterval !== undefined) {
+            comScoreParams.usagePropertiesAutoUpdateInterval =
+              params.usagePropertiesAutoUpdateInterval;
+          }
+
+          if (params.data_1p !== undefined) {
+            comScoreParams.data_1p = params.data_1p;
+          }
+
+          await ComScoreModule.initializeComScore(comScoreParams);
+
+          console.log('comScoreInitialized');
+        } catch (e) {
+          console.log('Error in initializing comscore', e);
+        }
+      }
+
+      async trackScreen(screenName: string) {
+        try {
+          if (screenName === undefined) return;
+          await ComScoreModule.trackScreen(screenName);
+        } catch (e) {
+          console.log('error in trackScreen : ', e);
+        }
+      }
+
+      async updateConsent(consentState: string) {
+        try {
+          await ComScoreModule.updateConsent(consentState);
+        } catch (e) {
+          console.log('Error in updateConsent', e);
+        }
+      }
+
+      async updateData1P(params: Data1p) {
+        let updatedData1p: Data1p = {};
+
+        if (params.publisherId !== undefined) {
+          updatedData1p.publisherId = params.publisherId;
+        } else {
+          return console.error('PublisherId is required!');
+        }
+
+        if (params.cs_fpid !== undefined) {
+          updatedData1p.cs_fpid = params.cs_fpid;
+        }
+
+        if (params.cs_fpdm !== undefined) {
+          updatedData1p.cs_fpdm = params.cs_fpdm;
+        }
+
+        if (params.cs_fpit !== undefined) {
+          updatedData1p.cs_fpit = params.cs_fpit;
+        }
+
+        if (params.cs_fpdt !== undefined) {
+          updatedData1p.cs_fpdt = params.cs_fpdt;
+        }
+
+        await ComScoreModule.update1PData(updatedData1p);
+      }
     }
 
-    if (params.usagePropertiesAutoUpdateMode !== undefined) {
-      comScoreParams.usagePropertiesAutoUpdateMode =
-        params.usagePropertiesAutoUpdateMode;
-    }
+    return new Comscore();
+  });
 
-    if (params.usagePropertiesAutoUpdateInterval !== undefined) {
-      comScoreParams.usagePropertiesAutoUpdateInterval =
-        params.usagePropertiesAutoUpdateInterval;
-    }
+  return {
+    initializeComscore: comscoreInstance.initializeComscore.bind(comscoreInstance),
+    trackScreen: comscoreInstance.trackScreen.bind(comscoreInstance),
+    updateConsent: comscoreInstance.updateConsent.bind(comscoreInstance),
+    updateData1P: comscoreInstance.updateData1P.bind(comscoreInstance),
+  };
+};
 
-    if (params.data_1p !== undefined) {
-      comScoreParams.data_1p = params.data_1p;
-    }
-
-    await ComScoreModule.initializeComScore(comScoreParams);
-
-    console.log('comScoreInitialized');
-  } catch (e) {
-    console.log('Error in initializing comscore', e);
-  }
-}
-
-export async function trackScreen(screenName: String) {
-  try {
-    if (screenName === undefined) return;
-    await ComScoreModule.trackScreen(screenName);
-  } catch (e) {
-    console.log('error in trackScreen : ', e);
-  }
-}
-
-export async function updateConsent(consentState: string) {
-  try {
-    await ComScoreModule.updateConsent(consentState);
-  } catch (e) {
-    console.log('Error in updateConsent', e);
-  }
-}
-
-export async function updateData1P(params: Data1p) {
-  let updatedData1p: Data1p = {};
-
-  if (params.publisherId !== undefined) {
-    updatedData1p.publisherId = params.publisherId;
-  } else {
-    return console.error('PublisherId is required!');
-  }
-
-  if (params.cs_fpid !== undefined) {
-    updatedData1p.cs_fpid = params.cs_fpid;
-  }
-
-  if (params.cs_fpdm !== undefined) {
-    updatedData1p.cs_fpdm = params.cs_fpdm;
-  }
-
-  if (params.cs_fpit !== undefined) {
-    updatedData1p.cs_fpit = params.cs_fpit;
-  }
-
-  if (params.cs_fpdt !== undefined) {
-    updatedData1p.cs_fpdt = params.cs_fpdt;
-  }
-
-  await ComScoreModule.update1PData(updatedData1p);
-}
+export default useComscore;
